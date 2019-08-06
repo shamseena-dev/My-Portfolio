ca0fb45a22a40444aeec39b057ae894adafe0838
shamseena-dev <shamseenakarumarot@gmail.com>
2019-08-06 19:45:15 +0200

    adding  files

---
+++ script.js
@@ -0,0 +1,15 @@
+const slideNavMenu=()=>{
+	
+	
+	const icon=document.querySelector('.iconSelect');
+	const navLinksDiv=document.querySelector('.navbar .navDiv')
+
+	icon.addEventListener('click',()=>{
+		icon.classList.toggle("closeOnly");
+		navLinksDiv.classList.toggle("navLinks-onScreen");
+		
+	})
+
+}
+
+slideNavMenu();
